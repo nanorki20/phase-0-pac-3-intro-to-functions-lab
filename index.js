fd8a0efc04825ccffb2b8d7951e8cac26e1e34da
Hayet Kumasseh <naaakleh@gmail.com>
2022-05-07 00:06:27 -0400

    commit tree e6bc4143f99af0506a275df2f13df14f7223598a

---
+++ index.js
@@ -0,0 +1,32 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout() {
+    console.log("HELLO");
+}
+
+function logWhisper() {
+    console.log("hello");
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) 
+    return("I can't hear you!")
+    if (string === string.toUpperCase())
+    return('YES INDEED!')
+    if (string === "Let's have dinner together!")
+    return('I would love to!')
+}
+
+shout("hello");
+whisper("HUSH");
+logShout();
+logWhisper();
+sayHiToHeadphonedRoommate("stop shouting");
+sayHiToHeadphonedRoommate("STOP SCREAMING");
+sayHiToHeadphonedRoommate("Let's have dinner together!");
